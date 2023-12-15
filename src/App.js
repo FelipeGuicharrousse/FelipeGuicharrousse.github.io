9af454ea31fd5b56dd2d4452b3ecdabca9e08514
FelipeGuicharrousse <felipe.guicharrousse@usm.cl>
2023-12-15 16:52:54 -0300

    🎉 First commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Pagina web de Felipe Guicharrousse
         </p>
         <a
           className="App-link"
